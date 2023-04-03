a63a16913fd3177775eb2e54d71008b1eafface5
rsshonjoydas <rsshonjoydas@gmail.com>
2023-04-03 15:07:35 +0600

    ci: create pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import ForgotPassword from './pages/ForgotPassword';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+
 function App() {
   return (
-    <div>
-      <h1>RS Shonjoy</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/offers' element={<Offers />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Offers.tsx
@@ -0,0 +1,3 @@
+const Offers = () => <div>Offers</div>;
+
+export default Offers;
