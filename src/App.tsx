e8ebd433b722686484da4c25f2c7f4883c92b0ff
rsshonjoydas <rsshonjoydas@gmail.com>
2023-04-04 17:56:36 +0600

    ci: react-toastify setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
@@ -9,17 +11,32 @@ import Register from './pages/Register';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/offers' element={<Offers />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/offers' element={<Offers />} />
+        </Routes>
+      </Router>
+
+      <ToastContainer
+        position='bottom-center'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+      />
+    </>
   );
 }
 
